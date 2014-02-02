e078aff8503eb46a8137313a41ea7c058b086e2d
Paul Fritsche <paul.fritsche@gmail.com>
2014-02-02 10:25:16 +0100

    EnemySystem to assign JobTargetComponents to enemies.

---
+++ src/main/java/org/terasology/ligthandshadow/componentsystem/controllers/EnemySystem.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2014 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.ligthandshadow.componentsystem.controllers;
+
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.entity.lifecycleEvents.BeforeRemoveComponent;
+import org.terasology.entitySystem.entity.lifecycleEvents.OnActivatedComponent;
+import org.terasology.entitySystem.event.ReceiveEvent;
+import org.terasology.entitySystem.systems.ComponentSystem;
+import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.jobSystem.JobTargetComponent;
+import org.terasology.ligthandshadow.componentsystem.components.LASTeam;
+import org.terasology.logic.characters.events.OnEnterBlockEvent;
+import org.terasology.logic.location.LocationComponent;
+import org.terasology.registry.CoreRegistry;
+
+import javax.vecmath.Vector3f;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by synopia on 01.02.14.
+ */
+@RegisterSystem
+public class EnemySystem implements ComponentSystem {
+    public static final Logger logger = LoggerFactory.getLogger(EnemySystem.class);
+    private Map<String, Set<EntityRef>> teams = Maps.newHashMap();
+    private Set<EntityRef> entities = Sets.newHashSet();
+    private Map<EntityRef, Set<Distance>> distances = Maps.newHashMap();
+
+    public EnemySystem() {
+        CoreRegistry.put(EnemySystem.class, this);
+    }
+
+    @Override
+    public void initialise() {
+    }
+
+    @ReceiveEvent
+    public void onActivated(OnActivatedComponent event, EntityRef entityRef, LASTeam team, LocationComponent locationComponent) {
+        logger.info("activated " + entityRef + " " + team.team);
+        Set<EntityRef> teamEntities = teams.get(team.team);
+        if (teamEntities == null) {
+            teamEntities = Sets.newHashSet();
+            teams.put(team.team, teamEntities);
+        }
+        teamEntities.add(entityRef);
+        entities.add(entityRef);
+//        recalculateDistances();
+        JobTargetComponent component = new JobTargetComponent();
+        component.jobUri = "LightAndShadow:attackMinion" + team.team;
+        entityRef.addComponent(component);
+    }
+
+    @ReceiveEvent
+    public void onRemoved(BeforeRemoveComponent event, EntityRef entityRef, LASTeam team, LocationComponent locationComponent, JobTargetComponent jobTargetComponent) {
+        logger.info("removed " + entityRef + " " + team.team);
+        entities.remove(entityRef);
+        Set<EntityRef> map = teams.get(team.team);
+        map.remove(entityRef);
+    }
+
+    @ReceiveEvent
+    public void onBlockChange(OnEnterBlockEvent event, EntityRef entityRef, LASTeam team) {
+        if (entities.contains(entityRef)) {
+//            recalculateDistances();
+        }
+    }
+
+    private void recalculateDistances() {
+        distances.clear();
+        Set<EntityRef> others = Sets.newHashSet(entities);
+        for (Map.Entry<String, Set<EntityRef>> entry : teams.entrySet()) {
+            others.removeAll(entry.getValue());
+            for (EntityRef one : entry.getValue()) {
+                for (EntityRef two : others) {
+                    Vector3f dist = one.getComponent(LocationComponent.class).getWorldPosition();
+                    dist.sub(two.getComponent(LocationComponent.class).getWorldPosition());
+                    float length = dist.length();
+                    addDistance(one, two, length);
+                    addDistance(two, one, length);
+                }
+            }
+        }
+    }
+
+    private void addDistance(EntityRef one, EntityRef two, float dist) {
+        Set<Distance> map = distances.get(one);
+        if (map == null) {
+            map = Sets.newTreeSet();
+            distances.put(one, map);
+        }
+        map.add(new Distance(two, dist));
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+    private static final class Distance implements Comparable<Distance> {
+        public float distance;
+        public EntityRef target;
+
+        private Distance(EntityRef target, float distance) {
+            this.target = target;
+            this.distance = distance;
+        }
+
+        @Override
+        public int compareTo(Distance o) {
+            return Float.compare(distance, o.distance);
+        }
+    }
+}
---
+++ src/main/java/org/terasology/ligthandshadow/jobs/AttackMinionSystem.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2014 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.ligthandshadow.jobs;
+
+import com.google.common.collect.Lists;
+import org.terasology.engine.SimpleUri;
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.systems.ComponentSystem;
+import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.jobSystem.Job;
+import org.terasology.jobSystem.JobFactory;
+import org.terasology.pathfinding.componentSystem.PathfinderSystem;
+import org.terasology.pathfinding.model.WalkableBlock;
+import org.terasology.registry.In;
+
+import java.util.List;
+
+/**
+ * Created by synopia on 01.02.14.
+ */
+@RegisterSystem
+public class AttackMinionSystem implements ComponentSystem {
+    @In
+    private PathfinderSystem pathfinderSystem;
+    @In
+    private JobFactory jobFactory;
+
+    public AttackMinionSystem() {
+    }
+
+    @Override
+    public void initialise() {
+        jobFactory.register(new AttackMinionJob("LightAndShadow:attackMinionBlack"));
+        jobFactory.register(new AttackMinionJob("LightAndShadow:attackMinionRed"));
+    }
+
+    @Override
+    public void shutdown() {
+    }
+
+
+    public class AttackMinionJob implements Job {
+        private final SimpleUri uri;
+
+        public AttackMinionJob(String uri) {
+            this.uri = new SimpleUri(uri);
+        }
+
+        @Override
+        public List<WalkableBlock> getTargetPositions(EntityRef enemy) {
+            List<WalkableBlock> targetPositions = Lists.newArrayList();
+            WalkableBlock walkableBlock = pathfinderSystem.getBlock(enemy);
+            if (walkableBlock != null) {
+                targetPositions.add(walkableBlock);
+            }
+
+            return targetPositions;
+        }
+
+        @Override
+        public boolean canMinionWork(EntityRef enemy, EntityRef minion) {
+            WalkableBlock actualBlock = pathfinderSystem.getBlock(minion);
+            WalkableBlock expectedBlock = pathfinderSystem.getBlock(enemy);
+
+            return actualBlock == expectedBlock;
+        }
+
+        @Override
+        public boolean isAssignable(EntityRef enemy) {
+            WalkableBlock walkableBlock = pathfinderSystem.getBlock(enemy);
+            return walkableBlock != null;
+        }
+
+        @Override
+        public void letMinionWork(EntityRef block, EntityRef minion) {
+
+        }
+
+        @Override
+        public boolean isRequestable(EntityRef block) {
+            return true;
+        }
+
+        @Override
+        public SimpleUri getUri() {
+            return uri;
+        }
+    }
+}
