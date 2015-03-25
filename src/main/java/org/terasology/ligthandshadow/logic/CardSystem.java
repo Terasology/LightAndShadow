af7f90797dab1d46cc8d26c7037c44aa14e26b14
Rasmus Praestholm <cervator@gmail.com>
2015-03-24 22:41:58 -0400

    Adapt as per recent engine Vector3i change (and occasional style fixes)

--- src/main/java/org/terasology/ligthandshadow/logic/CardSystem.java
+++ src/main/java/org/terasology/ligthandshadow/logic/CardSystem.java
@@ -32,7 +32,7 @@ import org.terasology.logic.location.LocationComponent;
 import org.terasology.logic.particles.BlockParticleEffectComponent;
 import org.terasology.math.Region3i;
 import org.terasology.math.Side;
-import org.terasology.math.Vector3i;
+import org.terasology.math.geom.Vector3i;
 import org.terasology.math.geom.Vector3f;
 import org.terasology.registry.In;
 import org.terasology.world.BlockEntityRegistry;
