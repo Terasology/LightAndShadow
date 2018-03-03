29d6a9c71a57d08943edfc7be44c8d43fa13a290
Devi Acharya <dacharya64@gmail.com>
2018-03-03 17:18:11 -0500

    Adds a rasterizer to place L&S flora in world on world generation

--- src/main/java/org/terasology/las/LaSWorldGenerator.java
+++ src/main/java/org/terasology/las/LaSWorldGenerator.java
@@ -16,20 +16,30 @@
 
 package org.terasology.las;
 
+import org.terasology.cities.BlockTheme;
 import org.terasology.cities.CityWorldGenerator;
+import org.terasology.cities.DefaultBlockType;
+import org.terasology.cities.SimpleBiomeProvider;
 import org.terasology.cities.bldg.Building;
 import org.terasology.cities.bldg.BuildingFacetProvider;
 import org.terasology.cities.blocked.BlockedAreaFacetProvider;
+import org.terasology.cities.deco.ColumnRasterizer;
+import org.terasology.cities.deco.DecorationFacetProvider;
+import org.terasology.cities.deco.SingleBlockRasterizer;
 import org.terasology.cities.door.DoorFacetProvider;
 import org.terasology.cities.door.SimpleDoorRasterizer;
 import org.terasology.cities.door.WingDoorRasterizer;
 import org.terasology.cities.fences.FenceFacetProvider;
 import org.terasology.cities.fences.SimpleFenceRasterizer;
+import org.terasology.cities.flora.FloraFacetProvider;
+import org.terasology.cities.flora.TreeFacetProvider;
 import org.terasology.cities.lakes.LakeFacetProvider;
 import org.terasology.cities.parcels.Parcel;
 import org.terasology.cities.parcels.ParcelFacetProvider;
+import org.terasology.cities.raster.standard.HollowBuildingPartRasterizer;
 import org.terasology.cities.raster.standard.RectPartRasterizer;
 import org.terasology.cities.raster.standard.RoundPartRasterizer;
+import org.terasology.cities.raster.standard.StaircaseRasterizer;
 import org.terasology.cities.roads.RoadFacetProvider;
 import org.terasology.cities.roads.RoadRasterizer;
 import org.terasology.cities.roof.ConicRoofRasterizer;
@@ -52,12 +62,15 @@ import org.terasology.cities.window.WindowFacetProvider;
 import org.terasology.commonworld.heightmap.HeightMap;
 import org.terasology.core.world.generator.facetProviders.DefaultFloraProvider;
 //import org.terasology.core.world.generator.facetProviders.EnsureSpawnableChunkZeroProvider;
+import org.terasology.core.world.generator.facetProviders.PerlinHumidityProvider;
 import org.terasology.core.world.generator.facetProviders.SeaLevelProvider;
 import org.terasology.core.world.generator.facetProviders.SurfaceToDensityProvider;
 import org.terasology.core.world.generator.rasterizers.FloraRasterizer;
 import org.terasology.core.world.generator.rasterizers.SolidRasterizer;
 
+import org.terasology.cities.SettlementEntityProvider;
 
+import org.terasology.core.world.generator.rasterizers.TreeRasterizer;
 import org.terasology.engine.SimpleUri;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.logic.spawner.Spawner;
@@ -71,6 +84,8 @@ import org.terasology.world.generation.WorldBuilder;
 import org.terasology.world.generator.RegisterWorldGenerator;
 import org.terasology.world.generator.plugin.WorldGeneratorPluginLibrary;
 
+import org.terasology.las.LaSFloraRasterizer;
+
 @RegisterWorldGenerator(id = "las", displayName = "Light & Shadow World")
 public class LaSWorldGenerator extends CityWorldGenerator {
 
@@ -80,6 +95,8 @@ public class LaSWorldGenerator extends CityWorldGenerator {
 
     @In
     private BlockManager blockManager;
+
+    private BlockTheme theme;
 
     /**
      * @param uri the uri
@@ -96,6 +113,92 @@ public class LaSWorldGenerator extends CityWorldGenerator {
 
     @Override
     protected WorldBuilder createWorld() {
-        return super.createWorld();
+        int seaLevel = 2;
+
+        theme = BlockTheme.builder(blockManager)
+                .register(DefaultBlockType.ROAD_FILL, "core:dirt")
+                .register(DefaultBlockType.ROAD_SURFACE, "core:Gravel")
+                .register(DefaultBlockType.LOT_EMPTY, "core:dirt")
+                .register(DefaultBlockType.BUILDING_WALL, "Cities:stonawall1")
+                .register(DefaultBlockType.BUILDING_FLOOR, "Cities:stonawall1dark")
+                .register(DefaultBlockType.BUILDING_FOUNDATION, "core:gravel")
+                .register(DefaultBlockType.TOWER_STAIRS, "core:CobbleStone")
+                .register(DefaultBlockType.ROOF_FLAT, "Cities:rooftiles2")
+                .register(DefaultBlockType.ROOF_HIP, "Cities:wood3")
+                .register(DefaultBlockType.ROOF_SADDLE, "Cities:wood3")
+                .register(DefaultBlockType.ROOF_DOME, "core:plank")
+                .register(DefaultBlockType.ROOF_GABLE, "core:plank")
+                .register(DefaultBlockType.SIMPLE_DOOR, BlockManager.AIR_ID)
+                .register(DefaultBlockType.WING_DOOR, BlockManager.AIR_ID)
+                .register(DefaultBlockType.WINDOW_GLASS, BlockManager.AIR_ID)
+                .register(DefaultBlockType.TOWER_WALL, "Cities:stonawall1")
+
+                // -- requires Fences module
+                .registerFamily(DefaultBlockType.FENCE, "Fences:Fence")
+                .registerFamily(DefaultBlockType.FENCE_GATE, BlockManager.AIR_ID)  // there is no fence gate :-(
+                .registerFamily(DefaultBlockType.TOWER_STAIRS, "core:CobbleStone:engine:stair")
+                .registerFamily(DefaultBlockType.BARREL, "StructuralResources:Barrel")
+                .registerFamily(DefaultBlockType.LADDER, "Core:Ladder")
+                .registerFamily(DefaultBlockType.PILLAR_BASE, "core:CobbleStone:StructuralResources:pillarBase")
+                .registerFamily(DefaultBlockType.PILLAR_MIDDLE, "core:CobbleStone:StructuralResources:pillar")
+                .registerFamily(DefaultBlockType.PILLAR_TOP, "core:CobbleStone:StructuralResources:pillarTop")
+                .registerFamily(DefaultBlockType.TORCH, "Core:Torch")
+
+                .build();
+
+        PerlinHumidityProvider.Configuration humidityConfig = new PerlinHumidityProvider.Configuration();
+        humidityConfig.octaves = 4;
+        humidityConfig.scale = 0.5f;
+
+        WorldBuilder worldBuilder = new WorldBuilder(CoreRegistry.get(WorldGeneratorPluginLibrary.class))
+                .setSeaLevel(seaLevel)
+                .addProvider(new SeaLevelProvider(seaLevel))
+                .addProvider(new InfiniteSurfaceHeightFacetProvider())
+                .addProvider(new SurfaceHeightFacetProvider())
+                .addProvider(new SurfaceToDensityProvider())
+                .addProvider(new BuildableTerrainFacetProvider())
+                .addProvider(new BlockedAreaFacetProvider())
+                .addProvider(new LakeFacetProvider())
+                .addProvider(new PerlinHumidityProvider(humidityConfig))
+                .addProvider(new SimpleBiomeProvider())
+                .addProvider(new SiteFacetProvider())
+                .addProvider(new TownWallFacetProvider())
+                .addProvider(new RoadFacetProvider())
+                .addProvider(new ParcelFacetProvider())
+                .addProvider(new FenceFacetProvider())
+                .addProvider(new WindowFacetProvider())
+                .addProvider(new DecorationFacetProvider())
+                .addProvider(new DoorFacetProvider())
+                .addProvider(new RoofFacetProvider())
+                .addProvider(new BuildingFacetProvider())
+                .addProvider(new SettlementFacetProvider())
+                .addProvider(new FloraFacetProvider())
+                .addProvider(new TreeFacetProvider())
+                .addRasterizer(new SolidRasterizer())
+                .addPlugins()
+                .addEntities(new SettlementEntityProvider())
+                .addRasterizer(new RoadRasterizer(theme))
+                .addRasterizer(new TownWallRasterizer(theme))
+                .addRasterizer(new SimpleFenceRasterizer(theme))
+                .addRasterizer(new RectPartRasterizer(theme))
+                .addRasterizer(new HollowBuildingPartRasterizer(theme))
+                .addRasterizer(new RoundPartRasterizer(theme))
+                .addRasterizer(new StaircaseRasterizer(theme))
+                .addRasterizer(new FlatRoofRasterizer(theme))
+                .addRasterizer(new SaddleRoofRasterizer(theme))
+                .addRasterizer(new PentRoofRasterizer(theme))
+                .addRasterizer(new HipRoofRasterizer(theme))
+                .addRasterizer(new ConicRoofRasterizer(theme))
+                .addRasterizer(new DomeRoofRasterizer(theme))
+                .addRasterizer(new SimpleWindowRasterizer(theme))
+                .addRasterizer(new RectWindowRasterizer(theme))
+                .addRasterizer(new SimpleDoorRasterizer(theme))
+                .addRasterizer(new WingDoorRasterizer(theme))
+                .addRasterizer(new SingleBlockRasterizer(theme))
+                .addRasterizer(new ColumnRasterizer(theme))
+                .addRasterizer(new LaSFloraRasterizer())
+                .addRasterizer(new TreeRasterizer());
+        return worldBuilder;
+        //return super.createWorld();
     }
 }
