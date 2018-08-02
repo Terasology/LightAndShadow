d055ab1912b54a7efcfe1bc0d259909a25490ab0
Devi Acharya <dacharya64@gmail.com>
2018-08-02 13:02:20 -0500

    Remove unused builder statement

--- src/main/java/org/terasology/ligthandshadow/componentsystem/controllers/TeleporterSystem.java
+++ src/main/java/org/terasology/ligthandshadow/componentsystem/controllers/TeleporterSystem.java
@@ -46,8 +46,6 @@ public class TeleporterSystem extends BaseComponentSystem {
     @In
     EntityManager entityManager;
 
-    private EntityBuilder builder;
-
     // The position near the team's base that player will be teleported to on choosing a team
     private static final Vector3f RED_TELEPORT_DESTINATION = new Vector3f(29, 12, 0);
     private static final Vector3f BLACK_TELEPORT_DESTINATION = new Vector3f(-29, 12, 0);
