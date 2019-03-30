cefc3615073057969bf9f13e0a477ea8e37df385
Darshan <darshantank3@gmail.com>
2019-03-30 12:04:15 +0530

    Added network component so that NPC is spawned at clients too

--- src/main/java/org/terasology/las/platform/GuidanceNpcProvider.java
+++ src/main/java/org/terasology/las/platform/GuidanceNpcProvider.java
@@ -21,6 +21,7 @@ import org.terasology.entitySystem.entity.EntityStore;
 import org.terasology.entitySystem.prefab.Prefab;
 import org.terasology.logic.location.LocationComponent;
 import org.terasology.math.geom.Vector3f;
+import org.terasology.network.NetworkComponent;
 import org.terasology.utilities.Assets;
 import org.terasology.world.generation.EntityBuffer;
 import org.terasology.world.generation.EntityProviderPlugin;
@@ -43,12 +44,13 @@ public class GuidanceNpcProvider implements EntityProviderPlugin {
             EntityStore entity = new EntityStore(chooseFactionNpc);
             entity.addComponent(new LocationComponent(new Vector3f(0, 61, 24)));
             entity.addComponent(dialogComp);
+            entity.addComponent(new NetworkComponent());
             buffer.enqueue(entity);
 
             Prefab beaconMark = Assets.getPrefab("BeaconMark").get();
             EntityStore entityStore = new EntityStore(beaconMark);
             entityStore.addComponent(new LocationComponent(new Vector3f(0, 63, 24)));
-
+            entityStore.addComponent(new NetworkComponent());
             buffer.enqueue(entityStore);
         }
 
