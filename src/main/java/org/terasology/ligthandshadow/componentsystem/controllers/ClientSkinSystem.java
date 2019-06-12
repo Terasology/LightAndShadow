5c7d7db87c65ae9fffa66bed0fcedddcdc45a589
Darshan <darshantank3@gmail.com>
2019-06-12 11:50:59 +0530

    Added logger

--- src/main/java/org/terasology/ligthandshadow/componentsystem/controllers/ClientSkinSystem.java
+++ src/main/java/org/terasology/ligthandshadow/componentsystem/controllers/ClientSkinSystem.java
@@ -15,7 +15,10 @@
  */
 package org.terasology.ligthandshadow.componentsystem.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.terasology.assets.management.AssetManager;
+import org.terasology.engine.modes.loadProcesses.AwaitedLocalCharacterSpawnEvent;
 import org.terasology.entitySystem.entity.EntityBuilder;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.entity.lifecycleEvents.OnChangedComponent;
@@ -26,8 +29,8 @@ import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
 import org.terasology.ligthandshadow.componentsystem.LASUtils;
 import org.terasology.ligthandshadow.componentsystem.components.LASTeamComponent;
-import org.terasology.ligthandshadow.componentsystem.events.SetPlayerHealthHUDEvent;
 import org.terasology.logic.characters.events.CreateVisualCharacterEvent;
+import org.terasology.logic.console.commandSystem.annotations.Command;
 import org.terasology.logic.players.LocalPlayer;
 import org.terasology.registry.In;
 import org.terasology.rendering.nui.NUIManager;
@@ -42,19 +45,13 @@ import org.terasology.utilities.Assets;
  */
 @RegisterSystem(RegisterMode.CLIENT)
 public class ClientSkinSystem extends BaseComponentSystem {
+    Logger logger = LoggerFactory.getLogger(ClientSkinSystem.class);
     @In
     private NUIManager nuiManager;
     @In
     private LocalPlayer localPlayer;
     @In
     private AssetManager assetManager;
-
-    @Override
-    public void initialise() {
-        HealthHud healthHud = nuiManager.getHUD().getHUDElement("core:healthHud", HealthHud.class);
-        healthHud.find("healthBar", UIIconBar.class).setIcon(Assets.getTextureRegion(LASUtils.getHealthIcon(LASUtils.WHITE_TEAM)).get());
-        healthHud.setSkin(Assets.getSkin(LASUtils.getHealthSkin(LASUtils.WHITE_TEAM)).get());
-    }
 
     /**
      * Updates the skeletal mesh of a player when its visual character is being created.
@@ -70,6 +67,8 @@ public class ClientSkinSystem extends BaseComponentSystem {
     @ReceiveEvent
     public void onCreateDefaultVisualCharacter(CreateVisualCharacterEvent event, EntityRef characterEntity,
                                                LASTeamComponent lasTeamComponent) {
+        logger.info("create default visual character");
+        logger.info(characterEntity.toFullDescription());
         Prefab prefab = assetManager.getAsset("engine:defaultVisualCharacter", Prefab.class).get();
         EntityBuilder entityBuilder = event.getVisualCharacterBuilder();
         entityBuilder.addPrefab(prefab);
@@ -79,19 +78,33 @@ public class ClientSkinSystem extends BaseComponentSystem {
         event.consume();
     }
 
-    /**
-     * Updates the skeletal mesh of a player when its team changes.
-     * @see LASTeamComponent
-     *
-     * @param event
-     * @param characterEntity
-     * @param lasTeamComponent
-     */
+    @ReceiveEvent
+    public void onAwaitedLocalCharacterSpawnEvent(AwaitedLocalCharacterSpawnEvent event, EntityRef characterEntity,
+                                                  LASTeamComponent lasTeamComponent) {
+        logger.info("local player spawned!");
+        setHealthHUD(lasTeamComponent.team);
+    }
+
+
+        /**
+         * Updates the skeletal mesh of a player when its team changes.
+         * @see LASTeamComponent
+         *
+         * @param event
+         * @param characterEntity
+         * @param lasTeamComponent
+         */
     @ReceiveEvent
     public void onLASTeamChange(OnChangedComponent event, EntityRef characterEntity, LASTeamComponent lasTeamComponent) {
-        if (!localPlayer.getCharacterEntity().equals(characterEntity) && characterEntity.hasComponent(VisualCharacterComponent.class)) {
+        logger.info("change team event received");
+        logger.info(String.valueOf(characterEntity.getOwner().equals(localPlayer.getClientEntity())));
+        if (characterEntity.hasComponent(VisualCharacterComponent.class)) {
             VisualCharacterComponent visualCharacterComponent = characterEntity.getComponent(VisualCharacterComponent.class);
             EntityRef visualCharacter = visualCharacterComponent.visualCharacter;
+            logger.info("changing character");
+            logger.info(characterEntity.toFullDescription());
+            logger.info(visualCharacter.toFullDescription());
+            logger.info(String.valueOf(visualCharacter.getOwner().equals(localPlayer.getClientEntity())));
             if (visualCharacter != EntityRef.NULL && visualCharacter.hasComponent(SkeletalMeshComponent.class)) {
                 SkeletalMeshComponent skeletalMeshComponent = visualCharacter.getComponent(SkeletalMeshComponent.class);
                 skeletalMeshComponent.material = Assets.getMaterial(LASUtils.getPlayerSkin(lasTeamComponent.team)).get();
@@ -100,21 +113,10 @@ public class ClientSkinSystem extends BaseComponentSystem {
         }
     }
 
-    /**
-     * Changes player health hud on receiving the corresponding event.
-     *
-     * @param event
-     * @param entity
-     */
-    @ReceiveEvent
-    public void onSetPlayerHealthHUDEvent(SetPlayerHealthHUDEvent event, EntityRef entity) {
-        EntityRef player = event.player;
-        String team = event.team;
-
-        if (player.getId() == localPlayer.getCharacterEntity().getId()) {
-            HealthHud healthHud = nuiManager.getHUD().getHUDElement("core:healthHud", HealthHud.class);
-            healthHud.find("healthBar", UIIconBar.class).setIcon(Assets.getTextureRegion(LASUtils.getHealthIcon(team)).get());
-            healthHud.setSkin(Assets.getSkin(LASUtils.getHealthSkin(team)).get());
-        }
+    private void setHealthHUD(String team) {
+        HealthHud healthHud = nuiManager.getHUD().getHUDElement("core:healthHud", HealthHud.class);
+        healthHud.find("healthBar", UIIconBar.class).setIcon(Assets.getTextureRegion(LASUtils.getHealthIcon(team)).get());
+        healthHud.setSkin(Assets.getSkin(LASUtils.getHealthSkin(team)).get());
     }
+
 }
