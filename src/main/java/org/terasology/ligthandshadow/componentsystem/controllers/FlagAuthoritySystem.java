3186ffe557f4f071859ada926219f312b4482f60
Darshan <darshantank3@gmail.com>
2019-06-23 03:48:08 +0530

    Add delayed action to flags on drop

---
+++ src/main/java/org/terasology/ligthandshadow/componentsystem/controllers/FlagAuthoritySystem.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2019 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.ligthandshadow.componentsystem.controllers;
+
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.event.ReceiveEvent;
+import org.terasology.entitySystem.systems.BaseComponentSystem;
+import org.terasology.entitySystem.systems.RegisterMode;
+import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.ligthandshadow.componentsystem.LASUtils;
+import org.terasology.logic.delay.DelayManager;
+import org.terasology.logic.inventory.ItemComponent;
+import org.terasology.logic.inventory.events.DropItemEvent;
+import org.terasology.registry.In;
+import org.terasology.world.block.items.BlockItemComponent;
+
+/**
+ * Handles events related to flag drops and pickups.
+ */
+@RegisterSystem(RegisterMode.AUTHORITY)
+public class FlagAuthoritySystem extends BaseComponentSystem {
+
+    @In
+    DelayManager delayManager;
+
+    /**
+     * Add a delayed action using delay manager to flags when they are dropped.
+     * @see DelayManager
+     *
+     * @param event
+     * @param itemEntity
+     * @param itemComponent
+     * @param blockItemComponent
+     */
+    @ReceiveEvent
+    public void onDropItemEvent(DropItemEvent event, EntityRef itemEntity, ItemComponent itemComponent,
+                                BlockItemComponent blockItemComponent) {
+        String blockFamilyURI = blockItemComponent.blockFamily.getURI().toString();
+        if (blockFamilyURI.equals(LASUtils.BLACK_FLAG_URI) || blockFamilyURI.equals(LASUtils.RED_FLAG_URI)) {
+            delayManager.addDelayedAction(itemEntity, LASUtils.DROPPED_FLAG, LASUtils.FLAG_TELEPORT_DELAY);
+        }
+    }
+}
