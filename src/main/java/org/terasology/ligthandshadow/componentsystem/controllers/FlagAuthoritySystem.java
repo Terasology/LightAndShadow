cefc0fadf98d21dfd6c844378bc41ee23507cc25
Darshan <darshantank3@gmail.com>
2019-06-23 04:09:18 +0530

    Add delay event handler to teleport flag

--- src/main/java/org/terasology/ligthandshadow/componentsystem/controllers/FlagAuthoritySystem.java
+++ src/main/java/org/terasology/ligthandshadow/componentsystem/controllers/FlagAuthoritySystem.java
@@ -22,9 +22,12 @@ import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
 import org.terasology.ligthandshadow.componentsystem.LASUtils;
 import org.terasology.logic.delay.DelayManager;
+import org.terasology.logic.delay.DelayedActionTriggeredEvent;
 import org.terasology.logic.inventory.ItemComponent;
 import org.terasology.logic.inventory.events.DropItemEvent;
 import org.terasology.registry.In;
+import org.terasology.world.WorldProvider;
+import org.terasology.world.block.BlockManager;
 import org.terasology.world.block.items.BlockItemComponent;
 
 /**
@@ -35,6 +38,12 @@ public class FlagAuthoritySystem extends BaseComponentSystem {
 
     @In
     DelayManager delayManager;
+
+    @In
+    WorldProvider worldProvider;
+
+    @In
+    BlockManager blockManager;
 
     /**
      * Add a delayed action using delay manager to flags when they are dropped.
@@ -51,6 +60,31 @@ public class FlagAuthoritySystem extends BaseComponentSystem {
         String blockFamilyURI = blockItemComponent.blockFamily.getURI().toString();
         if (blockFamilyURI.equals(LASUtils.BLACK_FLAG_URI) || blockFamilyURI.equals(LASUtils.RED_FLAG_URI)) {
             delayManager.addDelayedAction(itemEntity, LASUtils.DROPPED_FLAG, LASUtils.FLAG_TELEPORT_DELAY);
+        }
+    }
+    /**
+     * Destroy dropped items except flag after the delay set using delay manager.
+     * Flag is teleported back to the base.
+     * @see DelayManager
+     * @see DelayedActionTriggeredEvent
+     *
+     * @param event     The event which is triggered once the delay is over
+     * @param entity    The item on which the delay was set
+     */
+    @ReceiveEvent
+    public void destroyDroppedItems(DelayedActionTriggeredEvent event, EntityRef entity,
+                                    BlockItemComponent blockItemComponent) {
+        if (event.getActionId().equals(LASUtils.DROPPED_FLAG)) {
+            String blockFamilyURI = blockItemComponent.blockFamily.getURI().toString();
+            entity.destroy();
+            if (blockFamilyURI.equals(LASUtils.BLACK_FLAG_URI)) {
+                worldProvider.setBlock(LASUtils.getFlagLocation(LASUtils.BLACK_TEAM),
+                        blockManager.getBlock(LASUtils.getFlagURI(LASUtils.BLACK_TEAM)));
+            }
+            if (blockFamilyURI.equals(LASUtils.RED_FLAG_URI)) {
+                worldProvider.setBlock(LASUtils.getFlagLocation(LASUtils.RED_TEAM),
+                        blockManager.getBlock(LASUtils.getFlagURI(LASUtils.RED_TEAM)));
+            }
         }
     }
 }
