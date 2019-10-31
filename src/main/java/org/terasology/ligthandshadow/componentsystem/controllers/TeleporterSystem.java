ba98e862dd10f7f1b7b9923647d6ab49f4e92941
Tobias Nett <skaldarnar@googlemail.com>
2019-10-31 14:22:16 +0100

    Adapt usage of `MinimapIconComponent.iconUri` (renamed)

--- src/main/java/org/terasology/ligthandshadow/componentsystem/controllers/TeleporterSystem.java
+++ src/main/java/org/terasology/ligthandshadow/componentsystem/controllers/TeleporterSystem.java
@@ -69,7 +69,7 @@ public class TeleporterSystem extends BaseComponentSystem {
     private void setPlayerMinimapIcon(EntityRef player, String team) {
         MinimapIconComponent minimapIconComponent = player.getComponent(MinimapIconComponent.class);
         if (minimapIconComponent != null) {
-            minimapIconComponent.value = LASUtils.getMinimapIcon(team);
+            minimapIconComponent.iconUri = LASUtils.getMinimapIcon(team);
             player.saveComponent(minimapIconComponent);
         }
     }
