7f7203d6ae6531bba92ab391e1d9b4054bba6cf8
jdrueckert <jd.rueckert@googlemail.com>
2020-05-19 21:13:51 +0200

    chore: update core (spring cleanup) (#119)
    * chore: remove `Core` dependency, increase version
    * chore: add .gitattributes
    - also adds `CoreWorlds` dependency

--- src/main/java/org/terasology/las/LaSFloraRasterizer.java
+++ src/main/java/org/terasology/las/LaSFloraRasterizer.java
@@ -42,9 +42,9 @@ public class LaSFloraRasterizer extends FloraRasterizer {
         air = blockManager.getBlock(BlockManager.AIR_ID);
 
         flora.put(FloraType.GRASS, ImmutableList.<Block>of(
-                blockManager.getBlock("CoreBlocks:TallGrass1"),
-                blockManager.getBlock("CoreBlocks:TallGrass2"),
-                blockManager.getBlock("CoreBlocks:TallGrass3")));
+                blockManager.getBlock("CoreAssets:TallGrass1"),
+                blockManager.getBlock("CoreAssets:TallGrass2"),
+                blockManager.getBlock("CoreAssets:TallGrass3")));
 
         flora.put(FloraType.FLOWER, ImmutableList.<Block>of(
                 blockManager.getBlock("lightAndShadowResources:spadesCropSapling"),
@@ -55,9 +55,9 @@ public class LaSFloraRasterizer extends FloraRasterizer {
 
 
         flora.put(FloraType.MUSHROOM, ImmutableList.<Block>of(
-                blockManager.getBlock("CoreBlocks:BigBrownShroom"),
-                blockManager.getBlock("CoreBlocks:BrownShroom"),
-                blockManager.getBlock("CoreBlocks:RedShroom")));
+                blockManager.getBlock("CoreAssets:BigBrownShroom"),
+                blockManager.getBlock("CoreAssets:BrownShroom"),
+                blockManager.getBlock("CoreAssets:RedShroom")));
     }
 
 
