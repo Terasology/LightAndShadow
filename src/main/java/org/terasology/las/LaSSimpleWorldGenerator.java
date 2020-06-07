057f32fc6a1e8a804451c2f32acd635faa0558bb
Tobias Nett <skaldarnar@googlemail.com>
2020-06-07 15:17:31 +0200

    Remove dead code and update copy right headers

--- src/main/java/org/terasology/las/LaSSimpleWorldGenerator.java
+++ src/main/java/org/terasology/las/LaSSimpleWorldGenerator.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2017 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.las;
 
 import org.terasology.core.world.generator.facetProviders.DefaultFloraProvider;
@@ -76,7 +63,6 @@ public class LaSSimpleWorldGenerator extends BaseFacetedWorldGenerator {
                 .addProvider(new FloatingPlatformProvider())
                 .addPlugins()
                 .addRasterizer(new SolidRasterizer())
-//                .addRasterizer(new LaSSimpleWorldRasterizer())
                 .addRasterizer(new FloatingPlatformRasterizer())
                 .addRasterizer(new BaseRasterizer())
                 .addRasterizer(new LaSFloraRasterizer())
--- src/main/java/org/terasology/las/MountainsProvider.java
+++ src/main/java/org/terasology/las/MountainsProvider.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2019 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.las;
 
 import org.terasology.math.TeraMath;
@@ -20,20 +7,26 @@ import org.terasology.math.geom.BaseVector2i;
 import org.terasology.math.geom.Rect2i;
 import org.terasology.math.geom.Vector2f;
 import org.terasology.math.geom.Vector2i;
-import org.terasology.utilities.procedural.*;
-import org.terasology.world.generation.*;
+import org.terasology.utilities.procedural.BrownianNoise;
+import org.terasology.utilities.procedural.Noise;
+import org.terasology.utilities.procedural.SimplexNoise;
+import org.terasology.utilities.procedural.SubSampledNoise;
+import org.terasology.world.generation.Facet;
+import org.terasology.world.generation.FacetProvider;
+import org.terasology.world.generation.GeneratingRegion;
+import org.terasology.world.generation.Requires;
+import org.terasology.world.generation.Updates;
 import org.terasology.world.generation.facets.SurfaceHeightFacet;
 
 @Requires(@Facet(PlayAreaFacet.class))
 @Updates(@Facet(SurfaceHeightFacet.class))
 public class MountainsProvider implements FacetProvider {
     private Noise mountainNoise;
-//    private Noise hillNoise;
 
     @Override
     public void setSeed(long seed) {
-        mountainNoise = new SubSampledNoise(new BrownianNoise(new SimplexNoise(seed + 3)), new Vector2f(0.0002f, 0.0002f), 4);
-//        hillNoise = new SubSampledNoise(new BrownianNoise(new SimplexNoise(seed + 4)), new Vector2f(0.0008f, 0.0008f), 4);
+        mountainNoise = new SubSampledNoise(new BrownianNoise(new SimplexNoise(seed + 3)), new Vector2f(0.0002f,
+                0.0002f), 4);
     }
 
     @Override
@@ -41,21 +34,16 @@ public class MountainsProvider implements FacetProvider {
         SurfaceHeightFacet facet = region.getRegionFacet(SurfaceHeightFacet.class);
         PlayAreaFacet playAreaFacet = region.getRegionFacet(PlayAreaFacet.class);
         float mountainHeight = 40;
-        float hillHeight = 10;
-        // loop through every position on our 2d array
+
         Rect2i processRegion = facet.getWorldRegion();
         for (BaseVector2i position : processRegion.contents()) {
             // scale our max mountain height to noise (between -1 and 1)
             float additiveMountainHeight = mountainNoise.noise(position.x(), position.y()) * mountainHeight;
-//            float additiveHillHeight = hillNoise.noise(position.x(), position.y()) * hillHeight;
             // don't bother subtracting mountain height, that will allow unaffected regions
             additiveMountainHeight = TeraMath.clamp(additiveMountainHeight, 0, mountainHeight);
             if (!playAreaFacet.getWorld((Vector2i) position)) {
                 facet.setWorld(position, facet.getWorld(position) + additiveMountainHeight + 10);
             }
-//            else {
-//                facet.setWorld(position, facet.getWorld(position) + additiveHillHeight);
-//            }
         }
     }
 }
