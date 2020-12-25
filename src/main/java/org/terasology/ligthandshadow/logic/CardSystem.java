eff1764800dc0d0fefeab1765e0244a71c5f250a
Tobias Nett <skaldarnar@googlemail.com>
2020-12-25 12:20:17 +0100

    feat(BlockRegion): adjust for changes to BlockRegion (#179)

--- src/main/java/org/terasology/ligthandshadow/logic/CardSystem.java
+++ src/main/java/org/terasology/ligthandshadow/logic/CardSystem.java
@@ -43,7 +43,7 @@ import org.terasology.world.WorldProvider;
 import org.terasology.world.block.Block;
 import org.terasology.world.block.BlockComponent;
 import org.terasology.world.block.BlockManager;
-import org.terasology.world.block.BlockRegions;
+import org.terasology.world.block.BlockRegion;
 import org.terasology.world.block.family.BlockPlacementData;
 import org.terasology.world.block.regions.BlockRegionComponent;
 
@@ -127,7 +127,7 @@ public class CardSystem extends BaseComponentSystem {
 
         EntityRef cardEntity = entityManager.create(card.cardBlockPrefab);
         entity.removeComponent(MeshComponent.class);
-        cardEntity.addComponent(new BlockRegionComponent(BlockRegions.encompassing(bottomBlockPos, topBlockPos)));
+        cardEntity.addComponent(new BlockRegionComponent(new BlockRegion(bottomBlockPos).union(topBlockPos)));
 
         Vector3f cardCenter = new Vector3f(bottomBlockPos).add(0, 0.5f, 0);
         cardEntity.saveComponent(new LocationComponent(JomlUtil.from(cardCenter)));
