c3a704d5261478776e4f29b37af36a54b6185b8e
Michael Pollind <polli104@mail.chapman.edu>
2020-12-31 06:58:03 -0800

    feat(JOML): migrate MagicDomeSystem (#181)

--- src/main/java/org/terasology/ligthandshadow/barrier/MagicDomeSystem.java
+++ src/main/java/org/terasology/ligthandshadow/barrier/MagicDomeSystem.java
@@ -15,6 +15,8 @@
  */
 package org.terasology.ligthandshadow.barrier;
 
+import org.joml.Math;
+import org.joml.Vector3f;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terasology.audio.events.PlaySoundEvent;
@@ -29,9 +31,6 @@ import org.terasology.logic.characters.CharacterImpulseEvent;
 import org.terasology.logic.characters.CharacterMoveInputEvent;
 import org.terasology.logic.console.commandSystem.annotations.Command;
 import org.terasology.logic.location.LocationComponent;
-import org.terasology.math.JomlUtil;
-import org.terasology.math.TeraMath;
-import org.terasology.math.geom.Vector3f;
 import org.terasology.registry.In;
 import org.terasology.utilities.random.FastRandom;
 import org.terasology.world.RelevanceRegionComponent;
@@ -46,7 +45,7 @@ public class MagicDomeSystem extends BaseComponentSystem implements UpdateSubscr
     @In
     private EntityManager entityManager;
 
-    private Vector3f lastPos = Vector3f.zero();
+    private Vector3f lastPos = new Vector3f();
     private EntityRef magicDomeEntity = EntityRef.NULL;
     private float updateDelta;
     private FastRandom random = new FastRandom();
@@ -66,7 +65,7 @@ public class MagicDomeSystem extends BaseComponentSystem implements UpdateSubscr
 
         if (!entityManager.getEntitiesWith(MagicDome.class).iterator().hasNext()) {
 
-            magicDomeEntity = entityManager.create("lightAndShadowResources:magicDome", Vector3f.zero());
+            magicDomeEntity = entityManager.create("lightAndShadowResources:magicDome", new Vector3f());
             LocationComponent loc = magicDomeEntity.getComponent(LocationComponent.class);
             loc.setWorldScale(2 * WORLD_RADIUS * 1.01f);
             magicDomeEntity.addOrSaveComponent(loc);
@@ -85,16 +84,16 @@ public class MagicDomeSystem extends BaseComponentSystem implements UpdateSubscr
 
     @ReceiveEvent(components = {LocationComponent.class})
     public void onCharacterMovement(CharacterMoveInputEvent moveInputEvent, EntityRef player, LocationComponent loc) {
-        Vector3f pos = new Vector3f(loc.getWorldPosition());
+        Vector3f pos = new Vector3f(loc.getWorldPosition(new Vector3f()));
 
         float distance = pos.length();
 
-        float deltaDistance = TeraMath.fastAbs(pos.distance(lastPos));
+        float deltaDistance = Math.abs(pos.distance(lastPos));
 
         for (EntityRef domeEntity : entityManager.getEntitiesWith(MagicDome.class, LocationComponent.class)) {
             LocationComponent domeLocationComponent = domeEntity.getComponent(LocationComponent.class);
-            Vector3f domeCenter = domeLocationComponent.getWorldPosition();
-            Vector3f domeVerticalTop = domeCenter.addY(WORLD_RADIUS);
+            Vector3f domeCenter = domeLocationComponent.getWorldPosition(new Vector3f());
+            Vector3f domeVerticalTop = domeCenter.add(0, WORLD_RADIUS, 0);
 
             MagicDome dome = domeEntity.getComponent(MagicDome.class);
 
@@ -103,22 +102,23 @@ public class MagicDomeSystem extends BaseComponentSystem implements UpdateSubscr
 
                 if (lastPos.length() < WORLD_RADIUS && lastPos.length() < pos.length() && distance > WORLD_RADIUS) {
 //                    logger.info("Sending player back inside!");
-                    Vector3f impulse = pos.normalize().invert();
+                    Vector3f impulse = new Vector3f(pos).normalize().negate();
+                    impulse.mul(64.0f);
+                    impulse.y = 6;
 
-                    impulse.set(impulse.scale(64).setY(6));
-                    player.send(new CharacterImpulseEvent(JomlUtil.from(impulse)));
-
+                    player.send(new CharacterImpulseEvent(impulse));
                     player.send(new PlaySoundEvent(magicDomeEntity.getComponent(MagicDome.class).hitSound, 2f));
                 }
 
                 if (lastPos.length() > WORLD_RADIUS && lastPos.length() > pos.length() && distance < WORLD_RADIUS) {
 //                    logger.info("Sending player back outside");
-                    Vector3f impulse = pos.normalize();
-                    float verticalDiff = TeraMath.sqrt(TeraMath.fastAbs(TeraMath.sqr(lastPos.getY()) - TeraMath.sqr(domeVerticalTop.getY())));
-                    float impulseY = (TeraMath.fastAbs(verticalDiff) / (float) WORLD_RADIUS) * 3f;
+                    Vector3f impulse = new Vector3f(pos).normalize();
+                    float verticalDiff = Math.sqrt(Math.abs(Math.sqrt(lastPos.y()) - Math.sqrt(domeVerticalTop.y())));
+                    float impulseY = (Math.abs(verticalDiff) / (float) WORLD_RADIUS) * 3f;
 
-                    impulse.set(impulse.scale(64)).addY(impulseY);
-                    player.send(new CharacterImpulseEvent(JomlUtil.from(impulse)));
+                    impulse.mul(64);
+                    impulse.y += impulseY;
+                    player.send(new CharacterImpulseEvent(impulse));
 
                     player.send(new PlaySoundEvent(magicDomeEntity.getComponent(MagicDome.class).hitSound, 2f));
 
@@ -135,13 +135,12 @@ public class MagicDomeSystem extends BaseComponentSystem implements UpdateSubscr
             for (EntityRef entity : entityManager.getEntitiesWith(MagicDome.class, AnimateRotationComponent.class)) {
                 AnimateRotationComponent rotationComponent = entity.getComponent(AnimateRotationComponent.class);
 
-                rotationComponent.yawSpeed = TeraMath.clamp(rotationComponent.yawSpeed + random.nextFloat(-0.01f, 0.01f), -0.01f, 0.01f);
-                rotationComponent.pitchSpeed = TeraMath.clamp(rotationComponent.pitchSpeed + random.nextFloat(-0.01f, 0.01f), -0.01f, 0.01f);
-                rotationComponent.rollSpeed = TeraMath.clamp(rotationComponent.rollSpeed + random.nextFloat(-0.01f, 0.01f), -0.01f, 0.01f);
+                rotationComponent.yawSpeed = Math.clamp(-0.01f, 0.01f, rotationComponent.yawSpeed + random.nextFloat(-0.01f, 0.01f));
+                rotationComponent.pitchSpeed = Math.clamp(-0.01f, 0.01f, rotationComponent.pitchSpeed + random.nextFloat(-0.01f, 0.01f));
+                rotationComponent.rollSpeed = Math.clamp(-0.01f, 0.01f, rotationComponent.rollSpeed + random.nextFloat(-0.01f, 0.01f));
 
                 entity.saveComponent(rotationComponent);
             }
-
             updateDelta = 0;
         }
     }
