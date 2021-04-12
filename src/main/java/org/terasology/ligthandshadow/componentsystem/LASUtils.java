5d40b0cb013b05691a08557faa7c5fc0233bf076
Max J. Webber <tua83483@temple.edu>
2021-04-11 20:23:57 -0400

    Raised floating platform enough for it to be impossible for a tree to collide with/pierce it from below.

--- src/main/java/org/terasology/ligthandshadow/componentsystem/LASUtils.java
+++ src/main/java/org/terasology/ligthandshadow/componentsystem/LASUtils.java
@@ -29,10 +29,10 @@ public final class LASUtils {
     /**
      * Floating Platform values
      */
-    public static final Vector3ic FLOATING_PLATFORM_POSITION = new Vector3i(0, 60, 252);
+    public static final int FLOATING_PLATFORM_HEIGHT_LEVEL = 90;
+    public static final Vector3ic FLOATING_PLATFORM_POSITION = new Vector3i(0, FLOATING_PLATFORM_HEIGHT_LEVEL, 252);
     public static final int FLOATING_PLATFORM_WIDTH = 28;
     public static final int FLOATING_PLATFORM_LENGTH = 56;
-    public static final int FLOATING_PLATFORM_HEIGHT_LEVEL = 60;
     public static final int TELEPORTER_OFFSET = 4;
     public static final int NPC_OFFSET = 24;
     /**
