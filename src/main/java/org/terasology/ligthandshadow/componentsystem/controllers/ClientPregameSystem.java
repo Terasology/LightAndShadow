ca8f6cb7e44424531d60a99868155c811f92c55b
Harshit <av.harshith15@gmail.com>
2021-07-03 16:04:26 +0530

    remove timer

--- src/main/java/org/terasology/ligthandshadow/componentsystem/controllers/ClientPregameSystem.java
+++ src/main/java/org/terasology/ligthandshadow/componentsystem/controllers/ClientPregameSystem.java
@@ -7,10 +7,13 @@ import org.terasology.engine.entitySystem.systems.RegisterMode;
 import org.terasology.engine.entitySystem.systems.RegisterSystem;
 import org.terasology.engine.registry.In;
 import org.terasology.engine.rendering.nui.NUIManager;
+import org.terasology.gestalt.assets.ResourceUrn;
 import org.terasology.notify.ui.DialogNotificationOverlay;
 
 @RegisterSystem(RegisterMode.CLIENT)
 public class ClientPregameSystem extends BaseComponentSystem {
+
+    public static final ResourceUrn ASSET_URI = new ResourceUrn("LightAndShadow:Timer");
 
     @In
     private NUIManager nuiManager;
@@ -19,7 +22,8 @@ public class ClientPregameSystem extends BaseComponentSystem {
 
     @Override
     public void initialise() {
-        window = nuiManager.addOverlay(DialogNotificationOverlay.ASSET_URI, DialogNotificationOverlay.class);
+        window = nuiManager.addOverlay(ASSET_URI, DialogNotificationOverlay.class);
+        window.addNotification("The game starts in 10 seconds. \n The inventory will be reset.");
     }
 
     @Override
