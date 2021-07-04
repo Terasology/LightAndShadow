c66ec1c3446f0314fdde27eba19a1328008a4159
Harshit <52852877+ahv15@users.noreply.github.com>
2021-07-04 16:03:22 +0530

    feat: add pregame phase (#219)
    Creates a pregame phase which gives players time to join teams and setup their defenses.
    For the actual game to start there has to be at least one player in each team.

---
+++ src/main/java/org/terasology/ligthandshadow/componentsystem/events/PregameEvent.java
@@ -0,0 +1,11 @@
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
+package org.terasology.ligthandshadow.componentsystem.events;
+
+import org.terasology.engine.entitySystem.event.Event;
+
+/**
+ * Notification event that indicates the pregame phase has started.
+ */
+public class PregameEvent implements Event {
+}
