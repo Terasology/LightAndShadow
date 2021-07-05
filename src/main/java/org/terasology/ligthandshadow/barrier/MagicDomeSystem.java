7c1262f0640432154cac12b7890c5beadb23f55c
Tobias Nett <skaldarnar@googlemail.com>
2021-07-05 21:18:44 +0200

    Revert "feat: add pregame phase (#219)"
    This reverts commit c66ec1c3446f0314fdde27eba19a1328008a4159.
    
    The pre-game phase works well in local multiplayer, but we encountered
    more severe issues with it during the playtest event on 2021-07-04.
    Thus, reverting this commit temporarily for a stable release.

--- src/main/java/org/terasology/ligthandshadow/barrier/MagicDomeSystem.java
+++ src/main/java/org/terasology/ligthandshadow/barrier/MagicDomeSystem.java
@@ -1,99 +1,147 @@
-// Copyright 2021 The Terasology Foundation
-// SPDX-License-Identifier: Apache-2.0
+/*
+ * Copyright 2016 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.terasology.ligthandshadow.barrier;
 
-import org.terasology.engine.entitySystem.systems.RegisterMode;
+import org.joml.Math;
 import org.joml.Vector3f;
-import org.joml.Vector3ic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.terasology.engine.audio.events.PlaySoundEvent;
 import org.terasology.engine.entitySystem.entity.EntityManager;
 import org.terasology.engine.entitySystem.entity.EntityRef;
 import org.terasology.engine.entitySystem.event.ReceiveEvent;
 import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
 import org.terasology.engine.entitySystem.systems.RegisterSystem;
+import org.terasology.engine.entitySystem.systems.UpdateSubscriberSystem;
 import org.terasology.engine.logic.characters.CharacterImpulseEvent;
 import org.terasology.engine.logic.characters.CharacterMoveInputEvent;
+import org.terasology.engine.logic.console.commandSystem.annotations.Command;
 import org.terasology.engine.logic.location.LocationComponent;
 import org.terasology.engine.registry.In;
-import org.terasology.lightandshadowresources.components.LASTeamComponent;
-import org.terasology.ligthandshadow.componentsystem.LASUtils;
-import org.terasology.ligthandshadow.componentsystem.events.ActivateBarrierEvent;
-import org.terasology.ligthandshadow.componentsystem.events.DeactivateBarrierEvent;
+import org.terasology.engine.utilities.random.FastRandom;
+import org.terasology.engine.world.RelevanceRegionComponent;
+import org.terasology.itemRendering.components.AnimateRotationComponent;
 
-@RegisterSystem(RegisterMode.AUTHORITY)
-public class MagicDomeSystem extends BaseComponentSystem {
-    private static final int PREGAME_ZONE_RADIUS = 20;
+@RegisterSystem
+public class MagicDomeSystem extends BaseComponentSystem implements UpdateSubscriberSystem {
+
+    private static final Logger logger = LoggerFactory.getLogger(MagicDomeSystem.class);
+
+    private static final int WORLD_RADIUS = 500;
+
     @In
     private EntityManager entityManager;
 
-    private Vector3f position = new Vector3f();
-    private EntityRef redBarrier = EntityRef.NULL;
-    private EntityRef blackBarrier = EntityRef.NULL;
+    private Vector3f lastPos = new Vector3f();
+    private EntityRef magicDomeEntity = EntityRef.NULL;
+    private float updateDelta;
+    private FastRandom random = new FastRandom();
 
+    @Override
+    public void postBegin() {
+        //toggleDome();
+    }
 
-    /**
-     * Activates the barriers for the pregame regions corresponding to both the teams only in the beginning when
-     * the barriers haven't been created yet.
-     *
-     * @param event
-     * @param entity
-     */
-    @ReceiveEvent
-    public void activateBarriers(ActivateBarrierEvent event, EntityRef entity) {
-        if (redBarrier == EntityRef.NULL && blackBarrier == EntityRef.NULL) {
-            redBarrier = createBarrier("lightAndShadowResources:magicDome", LASUtils.CENTER_RED_BASE_POSITION, "red");
-            blackBarrier = createBarrier("lightAndShadowResources:magicDome", LASUtils.CENTER_BLACK_BASE_POSITION, "black");
+    @Command(shortDescription = "Activate/Deactivate dome barrier", helpText = "Activates or deactivates the dome barrier around the world", runOnServer = true)
+    public String dome() {
+        toggleDome();
+        return "Toggled dome.";
+    }
+
+    public void toggleDome() {
+
+        if (!entityManager.getEntitiesWith(MagicDome.class).iterator().hasNext()) {
+
+            magicDomeEntity = entityManager.create("lightAndShadowResources:magicDome", new Vector3f());
+            LocationComponent loc = magicDomeEntity.getComponent(LocationComponent.class);
+            loc.setWorldScale(2 * WORLD_RADIUS * 1.01f);
+            magicDomeEntity.addOrSaveComponent(loc);
+            magicDomeEntity.addOrSaveComponent(new RelevanceRegionComponent());
+            magicDomeEntity.setAlwaysRelevant(true);
+
+            AnimateRotationComponent rotationComponent = new AnimateRotationComponent();
+            rotationComponent.rollSpeed = 0.01f;
+            rotationComponent.pitchSpeed = 0.01f;
+            rotationComponent.yawSpeed = 0.01f;
+            magicDomeEntity.addOrSaveComponent(rotationComponent);
+        } else {
+            magicDomeEntity.destroy();
         }
     }
-
-    @ReceiveEvent
-    public void deactivateBarriers(DeactivateBarrierEvent event, EntityRef entity) {
-        redBarrier.destroy();
-        blackBarrier.destroy();
-    }
-
 
     @ReceiveEvent(components = {LocationComponent.class})
     public void onCharacterMovement(CharacterMoveInputEvent moveInputEvent, EntityRef player, LocationComponent loc) {
         Vector3f pos = new Vector3f(loc.getWorldPosition(new Vector3f()));
 
+        float distance = pos.length();
+
+        float deltaDistance = Math.abs(pos.distance(lastPos));
+
         for (EntityRef domeEntity : entityManager.getEntitiesWith(MagicDome.class, LocationComponent.class)) {
             LocationComponent domeLocationComponent = domeEntity.getComponent(LocationComponent.class);
             Vector3f domeCenter = domeLocationComponent.getWorldPosition(new Vector3f());
-            pos.sub(domeCenter, position);
-            float distance = position.length();
-            if (distance > PREGAME_ZONE_RADIUS && isAllowedInsideBarrier(player, domeEntity)) {
-                Vector3f impulse = position.normalize().negate();
-                impulse.mul(8);
+            Vector3f domeVerticalTop = domeCenter.add(0, WORLD_RADIUS, 0);
 
-                player.send(new CharacterImpulseEvent(impulse));
-                player.send(new PlaySoundEvent(domeEntity.getComponent(MagicDome.class).hitSound, 2f));
-            }
-            if (distance < PREGAME_ZONE_RADIUS && !isAllowedInsideBarrier(player, domeEntity)) {
-                Vector3f impulse = position.normalize();
-                impulse.mul(8);
-                player.send(new CharacterImpulseEvent(impulse));
+            MagicDome dome = domeEntity.getComponent(MagicDome.class);
 
-                player.send(new PlaySoundEvent(domeEntity.getComponent(MagicDome.class).hitSound, 2f));
+            if (deltaDistance > 0.2f) {
+//                logger.info("CharacerMoveInputEvent: position: {} - distance from O: {}, delta: {}", pos, distance, deltaDistance);
 
+                if (lastPos.length() < WORLD_RADIUS && lastPos.length() < pos.length() && distance > WORLD_RADIUS) {
+//                    logger.info("Sending player back inside!");
+                    Vector3f impulse = new Vector3f(pos).normalize().negate();
+                    impulse.mul(64.0f);
+                    impulse.y = 6;
+
+                    player.send(new CharacterImpulseEvent(impulse));
+                    player.send(new PlaySoundEvent(magicDomeEntity.getComponent(MagicDome.class).hitSound, 2f));
+                }
+
+                if (lastPos.length() > WORLD_RADIUS && lastPos.length() > pos.length() && distance < WORLD_RADIUS) {
+//                    logger.info("Sending player back outside");
+                    Vector3f impulse = new Vector3f(pos).normalize();
+                    float verticalDiff = Math.sqrt(Math.abs(Math.sqrt(lastPos.y()) - Math.sqrt(domeVerticalTop.y())));
+                    float impulseY = (Math.abs(verticalDiff) / (float) WORLD_RADIUS) * 3f;
+
+                    impulse.mul(64);
+                    impulse.y += impulseY;
+                    player.send(new CharacterImpulseEvent(impulse));
+
+                    player.send(new PlaySoundEvent(magicDomeEntity.getComponent(MagicDome.class).hitSound, 2f));
+
+                }
+                lastPos.set(pos);
             }
         }
     }
 
-    public EntityRef createBarrier(String prefab, Vector3ic location, String team) {
-        EntityRef barrier = entityManager.create(prefab, new Vector3f(location));
-        LocationComponent loc = barrier.getComponent(LocationComponent.class);
-        loc.setWorldScale(2 * PREGAME_ZONE_RADIUS * 1.01f);
-        MagicDome magicDome = barrier.getComponent(MagicDome.class);
-        magicDome.team = team;
-        barrier.addOrSaveComponent(loc);
-        barrier.addOrSaveComponent(magicDome);
-        return barrier;
-    }
+    @Override
+    public void update(float delta) {
+        updateDelta += delta;
+        if (updateDelta > 2.0f) {
+            for (EntityRef entity : entityManager.getEntitiesWith(MagicDome.class, AnimateRotationComponent.class)) {
+                AnimateRotationComponent rotationComponent = entity.getComponent(AnimateRotationComponent.class);
 
-    public boolean isAllowedInsideBarrier(EntityRef player, EntityRef barrier) {
-        String barrierTeam = barrier.getComponent(MagicDome.class).team;
-        String playerTeam = player.getComponent(LASTeamComponent.class).team;
-        return barrierTeam.equals(playerTeam);
+                rotationComponent.yawSpeed = Math.clamp(-0.01f, 0.01f, rotationComponent.yawSpeed + random.nextFloat(-0.01f, 0.01f));
+                rotationComponent.pitchSpeed = Math.clamp(-0.01f, 0.01f, rotationComponent.pitchSpeed + random.nextFloat(-0.01f, 0.01f));
+                rotationComponent.rollSpeed = Math.clamp(-0.01f, 0.01f, rotationComponent.rollSpeed + random.nextFloat(-0.01f, 0.01f));
+
+                entity.saveComponent(rotationComponent);
+            }
+            updateDelta = 0;
+        }
     }
 }
