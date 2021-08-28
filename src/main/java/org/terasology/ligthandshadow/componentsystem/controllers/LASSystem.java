8565489dd5b7a44c01fffe0e9b406d1f7531fdee
Harshit <av.harshith15@gmail.com>
2021-08-28 23:27:34 +0530

    override default economy handling

--- src/main/java/org/terasology/ligthandshadow/componentsystem/controllers/LASSystem.java
+++ src/main/java/org/terasology/ligthandshadow/componentsystem/controllers/LASSystem.java
@@ -2,45 +2,46 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.ligthandshadow.componentsystem.controllers;
 
-import org.terasology.engine.entitySystem.entity.EntityManager;
+import org.terasology.economy.components.CurrencyStorageComponent;
+import org.terasology.economy.events.WalletUpdatedEvent;
 import org.terasology.engine.entitySystem.entity.EntityRef;
 import org.terasology.engine.entitySystem.event.EventPriority;
 import org.terasology.engine.entitySystem.event.ReceiveEvent;
+import org.terasology.engine.entitySystem.prefab.Prefab;
 import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
 import org.terasology.engine.entitySystem.systems.RegisterSystem;
+import org.terasology.gestalt.assets.management.AssetManager;
 import org.terasology.module.inventory.components.InventoryComponent;
 import org.terasology.module.inventory.systems.InventoryManager;
 import org.terasology.module.inventory.events.RemoveItemAction;
 import org.terasology.engine.logic.players.event.OnPlayerSpawnedEvent;
 import org.terasology.engine.registry.In;
-import org.terasology.engine.world.WorldProvider;
-import org.terasology.engine.world.block.BlockManager;
-import org.terasology.engine.world.block.items.BlockItemFactory;
 import org.terasology.engine.world.sun.CelestialSystem;
 
 @RegisterSystem
 public class LASSystem extends BaseComponentSystem {
     @In
-    private EntityManager entityManager;
-    @In
     private InventoryManager inventoryManager;
     @In
-    private WorldProvider worldProvider;
-    @In
-    private BlockManager blockManager;
-    @In
     private CelestialSystem celestialSystem;
+    @In
+    private AssetManager assetManager;
 
     /**
-     * Gives an empty inventory to a player in the lobby to prevent fight's in the lobby.
+     * Gives an empty inventory to a player in the lobby to prevent fight's in the lobby and gives the player some funds.
      */
     @ReceiveEvent(priority = EventPriority.PRIORITY_LOW)
     public void onPlayerSpawn(OnPlayerSpawnedEvent event, EntityRef player, InventoryComponent inventory) {
-        BlockItemFactory blockFactory = new BlockItemFactory(entityManager);
         for (int i = 0; i < inventoryManager.getNumSlots(player); i++) {
             EntityRef itemInSlot = inventoryManager.getItemInSlot(player, i);
             player.send(new RemoveItemAction(player, itemInSlot, true));
         }
+
+        CurrencyStorageComponent component = assetManager.getAsset("engine:player", Prefab.class)
+                .map(prefab -> prefab.getComponent(CurrencyStorageComponent.class))
+                .orElse(new CurrencyStorageComponent());
+        player.addOrSaveComponent(component);
+        player.send(new WalletUpdatedEvent(component.amount));
     }
 
     @Override
--- src/main/java/org/terasology/ligthandshadow/componentsystem/controllers/RestartSystem.java
+++ src/main/java/org/terasology/ligthandshadow/componentsystem/controllers/RestartSystem.java
@@ -3,14 +3,18 @@
 package org.terasology.ligthandshadow.componentsystem.controllers;
 
 import org.joml.Vector3f;
+import org.terasology.economy.components.CurrencyStorageComponent;
+import org.terasology.economy.events.WalletUpdatedEvent;
 import org.terasology.engine.entitySystem.entity.EntityManager;
 import org.terasology.engine.entitySystem.entity.EntityRef;
 import org.terasology.engine.entitySystem.event.ReceiveEvent;
+import org.terasology.engine.entitySystem.prefab.Prefab;
 import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
 import org.terasology.engine.entitySystem.systems.RegisterMode;
 import org.terasology.engine.entitySystem.systems.RegisterSystem;
 import org.terasology.engine.logic.characters.CharacterTeleportEvent;
 import org.terasology.engine.logic.players.SetDirectionEvent;
+import org.terasology.gestalt.assets.management.AssetManager;
 import org.terasology.module.health.events.RestoreFullHealthEvent;
 import org.terasology.engine.network.ClientComponent;
 import org.terasology.engine.registry.In;
@@ -26,6 +30,8 @@ import org.terasology.lightandshadowresources.components.LASTeamComponent;
 public class RestartSystem extends BaseComponentSystem {
     @In
     EntityManager entityManager;
+    @In
+    private AssetManager assetManager;
 
     /**
      * All players' health are restored and they are transported back to their bases.
@@ -39,6 +45,11 @@ public class RestartSystem extends BaseComponentSystem {
         for (EntityRef client: clients) {
             EntityRef player = client.getComponent(ClientComponent.class).character;
             String team = player.getComponent(LASTeamComponent.class).team;
+            CurrencyStorageComponent component = assetManager.getAsset("engine:player", Prefab.class)
+                    .map(prefab -> prefab.getComponent(CurrencyStorageComponent.class))
+                    .orElse(new CurrencyStorageComponent());
+            player.addOrSaveComponent(component);
+            player.send(new WalletUpdatedEvent(component.amount));
             player.send(new RestoreFullHealthEvent(player));
             player.send(new CharacterTeleportEvent(LASUtils.getTeleportDestination(team)));
             player.send(new SetDirectionEvent(LASUtils.getYaw(LASUtils.getTeleportDestination(team).
