68a077276c68bdb0ae42bb52c96267cd34385c69
Josephine Rueckert <jd.rueckert@googlemail.com>
2021-11-21 10:45:20 +0100

    feat: add notification events for completed state transitions

---
+++ src/main/java/org/terasology/module/lightandshadow/events/OnCountdownPhaseStarted.java
@@ -0,0 +1,16 @@
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+package org.terasology.module.lightandshadow.events;
+
+import org.terasology.engine.network.BroadcastEvent;
+import org.terasology.gestalt.entitysystem.event.Event;
+
+// TODO: Would it make more sense to have a single "OnPhaseTransition" with old and new phase here?
+
+@BroadcastEvent
+public class OnCountdownPhaseStarted implements Event {
+
+    public OnCountdownPhaseStarted() {
+    }
+}
