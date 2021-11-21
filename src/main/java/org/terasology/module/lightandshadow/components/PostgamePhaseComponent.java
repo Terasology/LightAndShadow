79461030d3dde851c5d6c73040acc29b548ce1e8
Josephine Rueckert <jd.rueckert@googlemail.com>
2021-11-21 10:44:06 +0100

    feat: add phase components representing states

---
+++ src/main/java/org/terasology/module/lightandshadow/components/PostgamePhaseComponent.java
@@ -0,0 +1,10 @@
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+package org.terasology.module.lightandshadow.components;
+
+import org.terasology.gestalt.entitysystem.component.EmptyComponent;
+
+// TODO: Would it make sense to create a parent class `PhaseComponent` here?
+public class PostgamePhaseComponent extends EmptyComponent<PostgamePhaseComponent> {
+}
