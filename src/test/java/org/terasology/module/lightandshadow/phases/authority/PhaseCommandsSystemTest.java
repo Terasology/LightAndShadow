808f2c0630af8bf2385b4e8e8c62689bac1af3c4
Josephine Rueckert <jd.rueckert@googlemail.com>
2021-12-17 23:48:37 +0100

    fix: phase transition command test

--- src/test/java/org/terasology/module/lightandshadow/phases/authority/PhaseCommandsSystemTest.java
+++ src/test/java/org/terasology/module/lightandshadow/phases/authority/PhaseCommandsSystemTest.java
@@ -3,12 +3,18 @@
 
 package org.terasology.module.lightandshadow.phases.authority;
 
+import org.codehaus.plexus.util.cli.Arg;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.terasology.engine.entitySystem.entity.EntityRef;
 import org.terasology.engine.entitySystem.entity.internal.PojoEntityManager;
 import org.terasology.module.lightandshadow.phases.Phase;
+import org.terasology.module.lightandshadow.systems.GameEntitySystem;
+
+import java.util.stream.Stream;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -28,14 +34,31 @@ class PhaseCommandsSystemTest {
 
     private void createMockPhaseSystem(EntityRef gameEntity) {
         phaseCommandsSystem.phaseSystem = mock(PhaseSystem.class);
+        phaseCommandsSystem.phaseSystem.gameEntitySystem = mock(GameEntitySystem.class);
         when(phaseCommandsSystem.phaseSystem.gameEntitySystem.getGameEntity()).thenReturn(gameEntity);
     }
 
+    static Stream<Arguments> phaseProvider() {
+        Stream.Builder<Arguments> argumentsBuilder = Stream.builder();
+        for (Phase fromPhase : Phase.values()) {
+            for (Phase toPhase : Phase.values()) {
+                argumentsBuilder.add(Arguments.of(fromPhase, toPhase));
+            }
+        }
+
+        return argumentsBuilder.build();
+    }
+
     @ParameterizedTest
-    @EnumSource(Phase.class)
-    void testForceIdlePhaseSuccessful(Phase currentPhase) {
-        phaseCommandsSystem.forcePhase(EntityRef.NULL, "IDLE"); // TODO: use non-NULL entity
-        verify(phaseCommandsSystem.phaseSystem, times(1)).transitionPhase(currentPhase, Phase.IDLE);
+    @MethodSource("phaseProvider")
+    void testForceIdlePhaseSuccessful(Phase currentPhase, Phase targetPhase) {
+        // Arrange
+        when(phaseCommandsSystem.phaseSystem.getCurrentPhase()).thenReturn(currentPhase);
+        // Act
+        phaseCommandsSystem.forcePhase(EntityRef.NULL, targetPhase.toString()); // TODO: use non-NULL entity
+        // Assert
+        verify(phaseCommandsSystem.phaseSystem, times(1)).getCurrentPhase();
+        verify(phaseCommandsSystem.phaseSystem, times(1)).transitionPhase(currentPhase, targetPhase);
     }
 
 }
